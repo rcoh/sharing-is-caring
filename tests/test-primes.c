1e7bdb9cf32897a581002fa504ff5fff96c4fc36
Joseph Lynch <jolynch@mit.edu>
2012-12-10 22:51:20 -0500

    Fixed prime demo

--- tests/test-primes.c
+++ tests/test-primes.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include "../sic.h"
 
-#define SIZE 20000
+#define SIZE 10000000
 
 bool is_prime(int num) {
   int max_check = floor(sqrt(num)) + 1;
@@ -15,46 +15,48 @@ bool is_prime(int num) {
   return true;
 }
 
+// Take all the stack overflow code
+bool is_palindrome(int num) {
+  int n = num;
+  int rev = 0;
+  int dig;
+  while (num > 0) {
+    dig = num % 10;
+    rev = rev * 10 + dig;
+    num = num / 10;
+  }
+  return n == rev;
+}
+// And stop
+
 int main() {
   sic_init();
-  int i, j;
+  int i;
   char *primes = sic_malloc(SIZE);
   sic_barrier(0);
 
-  int max_check = floor(sqrt(SIZE)) + 1;
-  int length = max_check / sic_num_clients();
+  int length = SIZE / sic_num_clients();
   int begin = length * sic_id();
   if (begin == 0) {
     begin = 2;
   }
   int end = length * (sic_id()+1);
 
-  int num_computed = 3;
   for (i = begin; i < end ; i++) {
-    for (j = i+i; j < SIZE; j = j + i) {
-      primes[j] = 1;
-    }
-    num_computed++;
-    if(num_computed > 13) {
-      num_computed = 0;
-      sic_barrier(num_computed);
+    if(is_prime(i) && is_palindrome(i)) {
+      primes[i] = 1;
     }
   }
   sic_barrier(1);
 
-  int num_error = 0;
-  // Primes should have a 0 for primes and 1's for composite
-  for (i = 0; i < SIZE; i++) {
-    if(is_prime(i) && (primes[i] != 0)) {
-      num_error++;
-    } else if (!is_prime(i) && (primes[i] == 0)) {
-      num_error++;
+  if (sic_id() == 0) {
+    printf("Found: ");
+    for (i = 0; i < SIZE; i++) {
+      if (primes[i] == 1) {
+        printf("%d, ", i); 
+      }
     }
-  }
-  if(num_error == 0) {
-    printf("[SUCCESS] No errors primes all correct!\n");
-  } else {
-    printf("[ERROR] %d Prime computaiton errors!\n", num_error);
+    printf("\n");
   }
   sic_exit();
   return 0;
